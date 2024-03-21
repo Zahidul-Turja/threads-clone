17833f341b58b62aa78273daccc9d13f27e8c59e
Zahidul-Turja <zahidulturja@gmail.com>
2024-03-21 15:29:34 +0600

    initial commit

---
+++ frontend/src/components/UserHeader.jsx
@@ -0,0 +1,34 @@
+import { Avatar, Box, Flex, Text, VStack } from "@chakra-ui/react";
+
+function UserHeader() {
+  return (
+    <VStack gap={4} alignItems={"start"}>
+      <Flex justifyContent={"space-between"} w={"full"}>
+        <Box>
+          <Text fontSize={"2xl"} fontWeight={"bold"}>
+            Mark Zuckerberg
+          </Text>
+          <Flex gap={2} alignItems={"center"}>
+            <Text fontSize={"sm"}>markzuckerberg</Text>
+            <Text
+              fontSize={"xs"}
+              bg={"gray.dark"}
+              color={"gray.light"}
+              padding={1}
+              borderRadius={"full"}
+            >
+              threads.net
+            </Text>
+          </Flex>
+        </Box>
+        <Box>
+          <Avatar name="Mark Zuckerberg" src="/zuck-avatar.png" size={"xl"} />
+        </Box>
+      </Flex>
+
+      <Text>Co-founder, executive chairman and CEO of Meta Platforms.</Text>
+    </VStack>
+  );
+}
+
+export default UserHeader;
