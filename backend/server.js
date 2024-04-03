bdd3f23faef194ea29344ff44df8ce3767bd9331
Zahidul-Turja <zahidulturja@gmail.com>
2024-04-03 11:38:13 +0600

    authentication api created

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotev from "dotenv";
+import cookieParser from "cookie-parser";
 
 import connectDB from "./db/connectDB.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotev.config();
 connectDB();
@@ -9,6 +11,13 @@ connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); //! To parse JSON data in the req.body
+app.use(express.urlencoded({ extended: true })); //! To parse form data in the req.body
+app.use(cookieParser());
+
+// ! Routes
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () =>
   console.log(`Server started at http://localhost:${PORT}`)
