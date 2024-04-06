557362a048e0b64fa8eead2309204f71a097192e
Zahidul-Turja <zahidulturja@gmail.com>
2024-04-06 12:34:28 +0600

    get, create and delete post

---
+++ backend/controllers/postController.js
@@ -0,0 +1,75 @@
+import Post from "../models/postModel.js";
+import User from "../models/userModel.js";
+
+const createPost = async (req, res) => {
+  try {
+    const { postedBy, text, img } = req.body;
+
+    if (!postedBy || !text) {
+      return res
+        .status(400)
+        .json({ message: "Postedby and text fields are required" });
+    }
+
+    const user = await User.findById(postedBy);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (user._id.toString() !== req.user._id.toString()) {
+      return res.status(401).json({ message: "Unauthorized to create post" });
+    }
+
+    const maxLength = 500;
+    if (text.length > maxLength) {
+      return res
+        .status(400)
+        .json({ message: `Text must be less than ${maxLength} characters` });
+    }
+
+    const newPost = new Post({ postedBy, text, img });
+    await newPost.save();
+
+    res.status(201).json({ message: "Post created successfully", newPost });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+    console.log("Error in createPost");
+  }
+};
+
+const getPost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    res.status(200).json({ message: "Post found", post });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+    console.log("Error in getPost");
+  }
+};
+
+const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) return res.status(404).json({ message: "Post not found" });
+
+    if (post.postedBy.toString() !== req.user._id.toString()) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized to delete this post" });
+    }
+
+    await Post.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({ message: "Post deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+    console.log("Error in deletePost");
+  }
+};
+
+export { createPost, getPost, deletePost };
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 import connectDB from "./db/connectDB.js";
 import userRoutes from "./routes/userRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
 
 dotev.config();
 connectDB();
@@ -12,12 +13,14 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// ! Middlewares
 app.use(express.json()); //! To parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })); //! To parse form data in the req.body
 app.use(cookieParser());
 
 // ! Routes
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(PORT, () =>
   console.log(`Server started at http://localhost:${PORT}`)
